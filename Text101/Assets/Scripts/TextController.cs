8dda1d403cee3f72d32531edf67b4e094d1673c5
mahyar121 <mahyarhajibabaie@gmail.com>
2016-12-24 12:39:58 -0800

    Created text manipulation

---
+++ Text101/Assets/Scripts/TextController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+
+    public Text text;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // manipulating the text object to hello world
+        text.text = "Hello world";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            text.text = "Space key pressed";
+        }
+       
+	}
+}
